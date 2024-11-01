7fafefa07e6956b0f3e32cc39bf4540dd27c8301
Igor Nikolic <igor.nikolic@librafire.com>
2024-11-01 15:01:55 +0100

    BE - Certification section done

---
+++ blocks/certifications/certifications.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const closeModal = document.querySelectorAll(".close-modal");
+
+  document.querySelectorAll(".certifications-list-item").forEach((item) => {
+    item.addEventListener("click", function () {
+      const id = this.dataset.id;
+      const modal = document.getElementById(`certificationModal-${id}`);
+
+      if (modal) {
+        modal.style.display = "block";
+      }
+    });
+  });
+
+  closeModal.forEach((close) => {
+    close.addEventListener("click", function () {
+      const modal = this.closest(".modal");
+      if (modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+
+  window.addEventListener("click", function (event) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+});
---
+++ blocks/certifications/certifications.php
@@ -0,0 +1,115 @@
+<?php 
+/*
+* Block Name: Certifications
+* Post Type: page 
+*/
+
+$heading = get_field('heading');
+$sub_heading = get_field('sub_heading');
+$first_button = get_field('first_button');
+$certifications_heading = get_field('certifications_heading');
+$certification_subheading = get_field('certification_subheading');
+$certifications_cards = get_field('certifications_cards');
+$right_image = get_field('right_image');
+
+if (isset($block['data']['preview_image_help'])) :
+    echo '<img src="' . $block['data']['preview_image_help'] . '" style="width:100%; height:auto;">';
+else: ?>
+
+    <section class="certifications-wrapper">
+        <div class="container">
+            <div class="certifications-inner">
+                <div class="certifications-left">
+                    <div class="certifications-left-inner">
+                        <div class="box first">
+                            <?php if ($heading): ?>
+                            <h2 class="certifications-heading">
+                                <?php echo esc_html($heading); ?>
+                            </h2>
+                            <?php endif; ?>
+
+                            <?php if ($sub_heading): ?>
+                            <p class="certifications-subheading">
+                                <?php echo esc_html($sub_heading); ?>
+                            </p>
+                            <?php endif; ?>
+
+                            <?php if ($first_button): ?>
+                            <div class="button-wrapper">
+                                <a href="<?php echo esc_url($first_button['url']); ?>" <?php if ($first_button['target']) echo 'target="' . esc_attr($first_button['target']) . '"'; ?> class="btn">
+                                    <?php echo esc_html($first_button['title']); ?>
+                                    <span class="btn-image">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/btn-arrow.svg" alt="arrow" />
+                                    </span>                         
+                                </a>
+                            </div>
+                            <?php endif; ?>
+                        </div>
+                        <div class="box second">
+                            <div class="certifications-text">
+                                <?php if ($certifications_heading): ?> 
+                                <h2 class="certifications-heading">
+                                    <?php echo esc_html($certifications_heading); ?>
+                                </h2>
+                                <?php endif; ?>
+
+                                <?php if ($certification_subheading): ?>
+                                <p class="certifications-subheading">
+                                    <?php echo esc_html($certification_subheading); ?>
+                                </p>
+                                <?php endif; ?>
+                            </div>
+                            <div class="certifications-list">
+                                <?php foreach ($certifications_cards as $certification_card): ?>
+                                    <div class="certifications-list-item" data-id="<?php echo esc_attr($certification_card['id']); ?>" data-description="<?php echo esc_attr($certification_card['description']); ?>">     
+                                        <?php if ($certification_card['heading']): ?>                               
+                                            <h3 class="certifications-list-item-heading">
+                                                <?php echo esc_html($certification_card['heading']); ?>
+                                            </h3>
+                                        <?php endif; ?>
+
+                                        <?php if ($certification_card['sub_heading']): ?>
+                                            <p class="certifications-list-item-text">
+                                                <?php echo esc_html($certification_card['sub_heading']); ?>
+                                            </p>
+                                        <?php endif; ?>
+                                    </div>
+
+                                    <!-- Modal for each certification -->
+                                    <div id="certificationModal-<?php echo esc_attr($certification_card['id']); ?>" class="modal">
+                                        <div class="modal-content">
+                                            <span class="close-modal">&times;</span>
+                                            <div class="modal-content-inner">
+                                                <div class="left">
+                                                    <h2 class="modal-heading"><?php echo esc_html($certification_card['heading']); ?></h2>
+                                                    <p class="modal-description"><?php echo esc_html($certification_card['description']); ?></p>
+                                                    <div class="button-wrapper">
+                                                        <a href="<?php echo esc_url($certification_card['button']['url']); ?>" <?php if ($certification_card['button']['target']) echo 'target="' . esc_attr($certification_card['button']['target']) . '"'; ?> class="btn">
+                                                            <?php echo esc_html($certification_card['button']['title']); ?>
+                                                            <span class="btn-image">
+                                                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/btn-arrow.svg" alt="arrow" />
+                                                            </span>                         
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                                <div class="right">
+                                                    <img src="<?php echo esc_url($certification_card['image']['url']); ?>" alt="Image of: <?php echo esc_attr($certification_card['image']['title']); ?>">
+                                                </div>                                                                                      
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>                           
+                        </div>
+                    </div>
+                </div>
+                <div class="certifications-right">
+                    <?php if ($right_image): ?>
+                        <img src="<?php echo esc_url($right_image['url']); ?>" alt="Image of: <?php echo esc_attr($right_image['title']); ?>">
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>           
+    </section><!-- .certifications-wrapper-->   
+
+<?php endif; ?>
